d1b5aa17c8b144d40adf4b06a34fa867719871b8
Calisto Abel Mathias <calistomathias.work@gmail.com>
2025-11-18 21:08:06 +0530

    feat: fix constants
    Signed-off-by: Calisto Abel Mathias <calistomathias.work@gmail.com>

--- src/constants/arcium.ts
+++ src/constants/arcium.ts
@@ -8,12 +8,11 @@ import {
 } from '@arcium-hq/client';
 
 export const MXE_ARCIUM_X25519_PUBLIC_KEY: ArciumX25519PublicKey = new Uint8Array([
-        0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
-        0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
-        0x00, 0x00,
+        27, 146, 220, 227, 8, 51, 189, 69, 119, 116, 110, 176, 137, 108, 212, 154, 185, 95, 149, 7,
+        4, 186, 213, 240, 72, 99, 178, 235, 183, 45, 153, 36,
 ]) as ArciumX25519PublicKey;
 
-export const CLUSTER_OFFSET = 0;
+export const CLUSTER_OFFSET = 768109697;
 
 export const DEFAULT_SIGNING_MESSAGE = new TextEncoder().encode(
         'Umbra Privacy - do NOT sign this message unless you are using an application or integration with Umbra Privacy! Proceed cautiously as this signature will be used to derive sensitive information that can be used to control/transact/decrypt balances and funds from your Umbra Accounts.'
